c65e6fa8dea815f9fa6610c800777014e9f806dc
Luke O'Neil <luke.oneil@alumni.unc.edu>
2024-08-17 14:15:09 -0400

    Added Layout and SQL tables

--- youpick/schema.sql
+++ youpick/schema.sql
@@ -1,17 +1,30 @@
-DROP TABLE IF EXISTS user;
--- DROP TABLE IF EXISTS ...;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS picks;
+DROP TABLE IF EXISTS requests;
 
-CREATE TABLE user (
+CREATE TABLE users (
      id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
 );
 
--- CREATE TABLE ... (
---   id INTEGER PRIMARY KEY AUTOINCREMENT,
---   author_id INTEGER NOT NULL,
---   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
---   title TEXT NOT NULL,
---   body TEXT NOT NULL,
---   FOREIGN KEY (author_id) REFERENCES user (id)
--- );
+CREATE TABLE picks (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title TEXT NOT NULL UNIQUE,
+  body TEXT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE requests (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    request_id INTEGER NOT NULL,
+    receive_id INTEGER NOT NULL,
+    status TEXT CHECK(status IN('pending', 'accepted', 'rejected')) NOT NULL DEFAULT 'pending',
+    FOREIGN KEY (request_id) REFERENCES users (id),
+    FOREIGN KEY (receive_id) REFERENCES users (id),
+    UNIQUE (request_id, receive_id, status)
+    -- I used CHATGpt to help with the UNIQUE line as I didn't want duplicate requests
+    -- I also asked it how to ensure the status was one of 3 predefined values
+);
