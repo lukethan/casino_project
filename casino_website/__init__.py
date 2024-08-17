c3efb0896361c8eb589a7e5f59d0293c2b04ae95
Luke O'Neil <luke.oneil@alumni.unc.edu>
2024-08-16 21:55:04 -0400

    fixed auth route

--- casino_website/__init__.py
+++ casino_website/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -22,8 +23,14 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     @app.route("/test")
     def test():
         return("Success!")
+    
+
     
     return app
