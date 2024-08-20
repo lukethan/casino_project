86c20d38d7f84ce6c564e37f18582bf49058c26a
Luke O'Neil <luke.oneil@alumni.unc.edu>
2024-08-20 17:47:51 -0400

    Added Create page and fxns

--- tests/test_picks.py
+++ tests/test_picks.py
@@ -1,3 +1,15 @@
 import pytest
-from flask import g, session
 from youpick.db import get_db
+
+def test_index(client, auth):
+    response = client.get('/', follow_redirects=True)
+    assert b"Username" in response.data
+    assert b"Register" in response.data
+
+    auth.login()
+    response = client.get('/')
+    assert b'Logout' in response.data
+    assert b'You Pick!' in response.data
+    assert b'Welcome' in response.data
+    # assert b'test\nbody' in response.data
+    # assert b'href="/1/update"' in response.data
--- youpick/picks.py
+++ youpick/picks.py
@@ -11,5 +11,33 @@ bp = Blueprint('picks', __name__)
 @bp.route("/")
 @login_required
 def index():
-    if request.method == "GET":
-        return render_template("picks/index.html")
+    db = get_db()
+    picks = db.execute("SELECT * FROM picks").fetchall()
+    return render_template("picks/index.html", picks=picks)
+    
+@bp.route("/make", methods=('GET', 'POST'))
+@login_required
+def make():
+    if request.method == "POST":
+        title = request.form["pick_title"]
+        body = request.form["pick_body"]
+        error = None
+        if not title:
+            error = "Please add a title"
+        elif not body:
+            error = "Please add a body"
+        if error == None:
+            db = get_db()
+            db.execute('INSERT INTO picks (user_id, title, body)' 'VALUES(?,?,?)', (g.user["id"], title, body))
+            db.commit()
+            return redirect("/")
+        flash (error)
+    return render_template("picks/make.html")
+    
+@bp.route("/requests", methods=('GET', 'POST'))
+@login_required
+def requests():
+    if request.method == "POST":
+        ...    
+    return render_template("picks/requests.html")
+
