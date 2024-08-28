4d5aaf4dfac1bebd1dbd4d659ed03e53a6cf6bca
Luke O'Neil <luke.oneil@alumni.unc.edu>
2024-08-28 17:52:49 -0400

    overhaul test 2

--- youpick/db.py
+++ youpick/db.py
@@ -1,15 +1,19 @@
 import os
+import click
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
-# migrate = Migrate()
+migrate = Migrate()
 
-def init_app(app):
+def init_app(app: Flask):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')  # PostgreSQL URL
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional: Disable Flask-SQLAlchemy track modifications
+
     db.init_app(app)
     migrate.init_app(app, db)
-    
+
     @app.cli.command('init-db')
     def init_db_command():
         """Initialize the database."""
